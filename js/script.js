9a945809d8deba6c7e4e3eb10a232e2f10473fee
Laura <lstorella10@gmail.com>
2020-06-24 16:11:32 +0200

    Step #2: Creazione array di oggetti

--- js/script.js
+++ js/script.js
@@ -14,30 +14,70 @@ Step #3 : Dare la possibilità all’utente attraverso 3 prompt di aggiungere un
 
 
 
-// Step #1
-//  ---> Creo oggetto e sue proprietà
-var profiloStudente = {
-  'nome': 'Sara',
-  'cognome': 'Rossi',
-  'eta': 19,
-};
+// // Step #1
+// //  ---> Creo oggetto e sue proprietà
+// var profiloStudente = {
+//   'nome': 'Sara',
+//   'cognome': 'Rossi',
+//   'eta': 19,
+// },
 
-// modifico valore proprietà
-profiloStudente.eta = 20;
-// aggiungo nuova proprietà
-profiloStudente.matricola = '12345XYZ';
+// // modifico valore proprietà
+// profiloStudente.eta = 20;
+// // aggiungo nuova proprietà
+// profiloStudente.matricola = '12345XYZ';
 
 
-//  ---> Stampo proprietà con ciclo for-in
-console.log('Dati profilo Studente:');
-// stampo tutte le proprietà dell'oggetto
-for (var key in profiloStudente) {
-  console.log(key + ': ' + profiloStudente[key]);
+// //  ---> Stampo proprietà con ciclo for-in
+// console.log('Dati profilo Studente:');
+// // stampo tutte le proprietà dell'oggetto
+// for (var key in profiloStudente) {
+//   console.log(key + ': ' + profiloStudente[key]);
+// }
+
+
+// Step #2
+//  ---> Creo array di oggetti (elenco studenti)
+var elencoStudenti = [
+  {
+    'nome': 'Sara',
+    'cognome': 'Rossi',
+    'eta': 19,
+  },
+
+  {
+    'nome': 'Marco',
+    'cognome': 'Bianchi',
+    'eta': 21,
+  },
+
+  {
+    'nome': 'Gianna',
+    'cognome': 'Verdi',
+    'eta': 20,
+  },
+
+  {
+    'nome': 'Alessio',
+    'cognome': 'Neri',
+    'eta': 30,
+  },
+
+  {
+    'nome': 'Sandra',
+    'cognome': 'Russo',
+    'eta': 25,
+  },
+]
+
+
+//  ---> Stampo proprietà con ciclo for
+console.log('Elenco Studenti');
+for (var i = 0; i < elencoStudenti.length; i++) {
+
+  var singoloStudente = elencoStudenti[i];
+  console.log(singoloStudente.nome + ' ' + singoloStudente.cognome);
 }
-
-
-
-
 
 
 

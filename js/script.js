a2902f30347e4c9231782914e059e7b7d05f05d9
Laura <lstorella10@gmail.com>
2020-06-24 16:35:41 +0200

    modifica js

--- js/script.js
+++ js/script.js
@@ -14,26 +14,26 @@ Step #3 : Dare la possibilità all’utente attraverso 3 prompt di aggiungere un
 
 
 
-// // Step #1
-// //  ---> Creo oggetto e sue proprietà
-// var profiloStudente = {
-//   'nome': 'Sara',
-//   'cognome': 'Rossi',
-//   'eta': 19,
-// },
+// Step #1
+//  ---> Creo oggetto e sue proprietà
+var profiloStudente = {
+  'nome': 'Sara',
+  'cognome': 'Rossi',
+  'eta': 19,
+};
 
-// // modifico valore proprietà
-// profiloStudente.eta = 20;
-// // aggiungo nuova proprietà
-// profiloStudente.matricola = '12345XYZ';
+// modifico valore proprietà
+profiloStudente.eta = 20;
+// aggiungo nuova proprietà
+profiloStudente.matricola = '12345XYZ';
 
 
-// //  ---> Stampo proprietà con ciclo for-in
-// console.log('Dati profilo Studente:');
-// // stampo tutte le proprietà dell'oggetto
-// for (var key in profiloStudente) {
-//   console.log(key + ': ' + profiloStudente[key]);
-// }
+//  ---> Stampo proprietà con ciclo for-in
+console.log('Dati profilo Studente:');
+// stampo tutte le proprietà dell'oggetto
+for (var key in profiloStudente) {
+  console.log(key + ': ' + profiloStudente[key]);
+}
 
 
 // Step #2

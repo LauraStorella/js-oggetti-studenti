8d02c4e40dc3474f049de28efebb84df5b3cf68c
Laura <lstorella10@gmail.com>
2020-06-24 15:43:30 +0200

    Step #1: Creazione oggetto

--- js/script.js
+++ js/script.js
@@ -9,24 +9,31 @@ schermo attraverso il for in tutte le proprietà.
 
 Step #2 : Creare un array di oggetti di studenti. Ciclare su tutti gli studenti e stampare per ognuno nome e cognome
 
-Step #3 : Dare la possibilità all’utente attraverso 3 prompt di aggiungere un nuovo oggetto studente inserendo nell’ ordine: nome, cognome e età. 
- */
+Step #3 : Dare la possibilità all’utente attraverso 3 prompt di aggiungere un nuovo oggetto studente inserendo nell’ ordine: nome, cognome e età.  
+*/
+
 
 
 // Step #1
-var studentProfile = {
-  firstName : 'Sara',
-  lastName : 'Rossi',
-  age : 20,
+//  ---> Creo oggetto e sue proprietà
+var profiloStudente = {
+  'nome': 'Sara',
+  'cognome': 'Rossi',
+  'eta': 19,
 };
 
+// modifico valore proprietà
+profiloStudente.eta = 20;
+// aggiungo nuova proprietà
+profiloStudente.matricola = '12345XYZ';
 
 
-
-
-
-
-
+//  ---> Stampo proprietà con ciclo for-in
+console.log('Dati profilo Studente:');
+// stampo tutte le proprietà dell'oggetto
+for (var key in profiloStudente) {
+  console.log(key + ': ' + profiloStudente[key]);
+}
 
 
 

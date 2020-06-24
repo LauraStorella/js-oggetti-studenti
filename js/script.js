7892f87228b39617c8d2d04eddadb7a08159676f
Laura <lstorella10@gmail.com>
2020-06-24 14:55:20 +0200

    JS | Objects & Handlebars

---
+++ js/script.js
@@ -0,0 +1,56 @@
+$(document).ready(function () {
+
+// *************** OBJECTS ***************
+
+/* 
+Step #1 : Creare un oggetto che descriva uno studente con le 
+seguenti proprietà: nome, cognome e età. Stampare a 
+schermo attraverso il for in tutte le proprietà.
+
+Step #2 : Creare un array di oggetti di studenti. Ciclare su tutti gli studenti e stampare per ognuno nome e cognome
+
+Step #3 : Dare la possibilità all’utente attraverso 3 prompt di aggiungere un nuovo oggetto studente inserendo nell’ ordine: nome, cognome e età. 
+ */
+
+
+// Step #1
+var studentProfile = {
+  firstName : 'Sara',
+  lastName : 'Rossi',
+  age : 20,
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}); // document ready
